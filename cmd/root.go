dd1fd4823c4fcf0f74a35a4932e19ea86abe7804
sumo <sumo@truecircle.ai>
2022-08-04 01:08:58 +0100

    removed default fn + Execute on call

--- cmd/root.go
+++ cmd/root.go
@@ -10,13 +10,10 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "dignore",
 	Short: "dignore list paths included / excluded by your dockerignore files",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cobra ran")
-	},
 }
 
 func Execute() {
-	if err := rootCmd.Help(); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
