ac10a001239e47631d59d6e99b24c2cd070842a0
sumo <sirapoab.c@gmail.com>
2022-12-05 21:10:29 +0000

    feat: edit lib to piggy-back from what docker is using

--- list_backend/export_test.go
+++
@@ -1,8 +0,0 @@
-package list_backend
-
-type MatchMode = matchMode
-
-var MatchInclude = matchInclude
-var MatchExclude = matchExclude
-var MatchNone = matchNone
-var CheckPathAgainstLine = checkPathAgainstLine
